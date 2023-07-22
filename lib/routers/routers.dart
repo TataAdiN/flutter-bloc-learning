2cf3a49a3f8f95ff83bf698f37b9264e10c6807c
tataadin <tataa2237@gmail.com>
2023-07-22 09:18:56 +0700

    [routes] fix route name

--- lib/routers/routers.dart
+++ lib/routers/routers.dart
@@ -1,4 +1,4 @@
-import 'package:bloc_learning/modules/detail/detail_page.dart';
+import 'package:bloc_learning/modules/auth/auth_page.dart';
 import 'package:bloc_learning/modules/home/home_page.dart';
 import 'package:bloc_learning/modules/post/post_page.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +14,27 @@ class Routers {
         },
         routes: <RouteBase>[
           GoRoute(
-            path: 'detail',
+            name: 'auth',
+            path: 'auth',
+            pageBuilder: (BuildContext context, GoRouterState state) {
+              return CustomTransitionPage(
+                  child: const AuthPage(),
+                  transitionsBuilder: (BuildContext context,
+                      Animation<double> animation,
+                      Animation<double> secondaryAnimation,
+                      Widget child) {
+                    return SlideTransition(
+                        position: Tween<Offset>(
+                          begin: const Offset(-1.0, 0.0),
+                          end: Offset.zero,
+                        ).animate(animation),
+                        child: child);
+                  });
+            },
+          ),
+          GoRoute(
+            name: 'post',
+            path: 'post',
             pageBuilder: (BuildContext context, GoRouterState state) {
               return CustomTransitionPage(
                   child: const PostPage(),
@@ -33,6 +53,6 @@ class Routers {
           ),
         ],
       ),
-    ]);
+    ], redirect: (BuildContext context, GoRouterState state) {});
   }
 }
