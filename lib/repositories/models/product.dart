ca9b2a74acfcccb82841f6193cf9e0b702422b0a
tataadin <tataa2237@gmail.com>
2023-07-30 18:48:12 +0700

    [refactor][model.product] change to product

--- lib/repositories/models/product.dart
+++ lib/repositories/models/product.dart
@@ -1,30 +1,61 @@
-class Post {
-  int userId, id;
-      String title, body;
-  Post(
-      {required this.userId,
+class Product {
+  int id, price, stock;
+  double discountPercentage, rating;
+  String title, description, brand, category, thumbnail;
+  List<dynamic> images;
+  Product(
+      {
       required this.id,
       required this.title,
-      required this.body});
+      required this.description,
+      required this.price,
+      required this.stock,
+      required this.discountPercentage,
+      required this.rating,
+      required this.brand,
+      required this.category,
+      required this.thumbnail,
+      required this.images});
 
-  factory Post.fromJson(Map<String, dynamic> json) => Post(
-    userId: json['userId'],
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
     id: json['id'],
     title: json['title'],
-    body: json['body'],
+      description : json['description'],
+      price : json['price'],
+      stock : json['stock'],
+      discountPercentage : json['discountPercentage'],
+      rating : json['rating'] is double ? json['rating'] : json['rating'].roundToDouble(),
+      brand : json['brand'],
+      category : json['category'],
+      thumbnail : json['thumbnail'],
+      images : json['images']
   );
 
-  Post copyWith({
-    required int userId,
+  Product copyWith({
     required int id,
     required String title,
-    required String body,
+    required String description,
+    required int price,
+    required int stock,
+    required double discountPercentage,
+    required double rating,
+    required String brand,
+    required String category,
+    required String thumbnail,
+    required List<String> images
   }) {
-    return Post(
-      userId: userId,
-      id: id,
-      title: title,
-      body: body,
+    return Product(
+        id : id,
+        title : title,
+        description : description,
+        price : price,
+        stock : stock,
+        discountPercentage : discountPercentage,
+        rating : rating,
+        brand : brand,
+        category : category,
+        thumbnail : thumbnail,
+        images : images
     );
   }
 }
